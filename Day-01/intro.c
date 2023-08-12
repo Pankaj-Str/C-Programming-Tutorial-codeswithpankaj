e15060a2e718431a4d2833e7a8e910aa743dfe6a
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-12 12:19:14 +0530

    Create intro.c

---
+++ Day-01/intro.c
@@ -0,0 +1,15 @@
+// header file
+#include <stdio.h>
+
+int main() 
+{ 
+
+  printf("www.p4n.in\n");
+  printf("www.p4n.in\n");
+  printf("%d\n",45);
+  printf("%d\n",(45+90));
+  
+  
+  return 0; 
+
+}
