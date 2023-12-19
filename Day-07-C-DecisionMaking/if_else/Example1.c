591d32c3395e391440fe812f245eacdfed2432da
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-12-19 04:21:46 +0000

    if else ...

---
+++ Day-07-C-DecisionMaking/if_else/Example1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+
+    int number1 = 100;
+    int number2 = 20;
+
+    if(number1 < number2){
+        printf("Number 1 is smallest...."); // t
+    }else{
+        printf("Number 1 not smallest...."); // f
+    }
+
+
+
+    return 0;
+}
