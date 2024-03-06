8de51cc648052c8717f49b68f518f0e92ad7cbdd
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-03-06 15:16:20 +0530

    update - DataType and variables

---
+++ Examples/basic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("Hello World! \n");  //Print Hello World to the screen
+    printf("codeswithpankaj.com \n");   //prints www.codeswithpankaj.com
+
+    printf("%d",900);
+
+    return 0;
+}
