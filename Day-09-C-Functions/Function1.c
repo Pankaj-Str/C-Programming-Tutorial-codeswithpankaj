74cfa58be1504fd861562c6c8eaadb73c3c53f5a
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-03-22 18:25:26 +0530

    Add files via upload

---
+++ Day-09-C-Functions/Function1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// basic function without arg. without return
+void info(){
+   printf("www.codeswithpankaj.com\n"); 
+}
+int main(){
+
+    // calling function
+    info();
+    info();
+
+
+
+    return 0;
+}
