64758df96582040743ebd697e8ba5ff5a1a5d3ba
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-03-28 14:54:10 +0530

    Update MultiArray_1.c

--- Day-12-C-Arrays/MultiArray/MultiArray_1.c
+++ Day-12-C-Arrays/MultiArray/MultiArray_1.c
@@ -2,32 +2,29 @@
 
 int main(){
 
+    // create a multi array
     int data[3][3];
 
-    data[0][0] = 23;
-    data[1][0] = 21;
-    data[2][0] = 22;
+    // assign values to the array
+    data[0][0] = 1;
+    data[0][1] = 2;
+    data[0][2] = 3;
+    data[1][0] = 4;
+    data[1][1] = 5;
+    data[1][2] = 6;
+    data[2][0] = 7;
+    data[2][1] = 8;
+    data[2][2] = 9;
+    
+    // print the array using nested for loop
 
-    data[0][1] = 24;
-    data[1][1] = 25;
-    data[2][1] = 26;
-
-    data[0][2] = 73;
-    data[1][2] = 78;
-    data[2][2] = 89;
-
-
-    //printf("%d\n",data[1][2]);
-
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%d ",data[i][j]);
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            printf("%d ", data[i][j]);
         }
         printf("\n");
     }
-    
+
 
     return 0;
 }
