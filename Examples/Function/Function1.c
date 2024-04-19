7a58d150fb98868029b367b70df1746b00777150
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-04-19 13:05:54 +0000

    Update launch.json with new paths

---
+++ Examples/Function/Function1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// basic function declaration
+// no return type and no arguments
+
+void printMessage();
+
+// defining the function
+
+void printMessage()
+{
+    printf("Hello World\n");
+}
+
+int main()
+{
+    // calling the function
+    printMessage();
+    return 0;
+}
