cff6d55de2edf31ec52fb7817197fcbaeac907ba
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-05-24 13:22:30 +0000

    find smallest number ...

---
+++ Day-07-C-DecisionMaking/if_else/Find_Smallest_No.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// header files - stdio.h
+
+int main(){
+
+    // find smallest number ...
+    int a = 10;
+    int b = 2;
+
+    if(a < b){
+        printf("Smallest number is %d\n", a);
+    }else{
+        printf("Smallest number is %d\n", b);
+    }
+
+    return 0;
+}
+
+
