9349792fbe6eae67819f037cd3267c9fb8e2d144
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-05-25 13:37:24 +0000

    update datatype

---
+++ Day-02-C-DataTypes/DataType.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    // Data type char
+
+    char name[] = "Joy";
+    char ltr = 'A';
+    int age = 12;
+    float height = 4.5;
+
+    // print all variables
+
+    printf("Your name is : %s\n",name);
+    printf("Your letter is : %c\n",ltr);
+    printf("Your age is : %d\n",age);
+    printf("Your height is : %f\n",height);
+
+    return 0;
+}
