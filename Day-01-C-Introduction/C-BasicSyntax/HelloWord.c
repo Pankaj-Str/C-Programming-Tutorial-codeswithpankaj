b00c32012320d077c8db66a965294d3f1ad30f70
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-05-25 13:32:11 +0000

    update helloword program

---
+++ Day-01-C-Introduction/C-BasicSyntax/HelloWord.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("welcome to codeswithpankaj.com\n");
+
+    // print numbers
+    printf("%d\n",9990);
+
+    // print float number
+    printf("%f\n",3.14);
+
+    // print char
+    printf("%c\n",'A');
+
+    
+
+    return 0;
+}
