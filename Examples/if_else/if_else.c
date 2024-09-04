3a4d661ad44bf6d7fe2ae97ba979c66991b6f3c6
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-09-04 06:42:34 +0000

    update if else example

---
+++ Examples/if_else/if_else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    // find the smallest number 
+
+    int number1 = 100;
+    int number2 = 20;
+
+
+    if(number1 < number2){
+        printf("Number 1 is smallest ....");
+    }else{
+        printf("Number 2 is smallest.... ");
+    }
+
+    return 0;
+}
