0cd179feceec226bf46d7e3ff854a47cbbd69cb1
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-09-20 10:01:59 +0000

    update array

---
+++ Examples/Arrays/SingleArray/ArrayExample.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int data[5];
+
+    data[0] = 90;
+    data[1] = 40;
+    data[2] = 50;
+    data[3] = 60;
+    data[4] = 70;
+
+    //printf("%d",data[3]);
+    
+    for(int a = 0;a<5;a++){
+
+        printf("%d\n",data[a]);
+    
+    }
+
+
+
+
+    return 0;
+}
+
