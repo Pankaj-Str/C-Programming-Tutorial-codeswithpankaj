49b9c753dde08003ade481ecd77b16ed0eb3e41d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-04 09:52:32 +0000

    update 2d array

--- Examples/Arrays/multiarray/multiarray.c
+++ Examples/Arrays/multiarray/multiarray.c
@@ -16,9 +16,22 @@ int main(){
     {
         for (int a = 0; a < col; a++)
         {
-            printf("Enter item into array  %d l %d\n",i,a);
+            printf("Enter item into array  %d | %d\n",i,a);
             scanf("%d",&data[i][a]);
         }
+        
+    }
+
+    printf("\n---------------out put-------------------\n");
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int a = 0; a < col; a++)
+        {
+            printf("%d ",data[i][a]);
+            
+        }
+        printf("\n");
         
     }
     
