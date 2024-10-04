72de92cb577055a60274b6627983f6d9608f5e40
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-04 09:18:56 +0000

    create array example

---
+++ Examples/Arrays/SingleArray/Array01.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+
+    printf("\n*******************************************\n");
+    printf("\n**                                     **\n");
+
+    printf("Enter array size \n");
+    int size;
+    scanf("%d",&size);
+
+    
+
+
+
+
+
+
+   
+
+
+    return 0;
+}
