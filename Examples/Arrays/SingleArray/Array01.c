d3b9cb0277afdd7264310f3201fbc470a081d63b
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-04 09:21:48 +0000

    user input into single array

--- Examples/Arrays/SingleArray/Array01.c
+++ Examples/Arrays/SingleArray/Array01.c
@@ -10,7 +10,21 @@ int main(){
     int size;
     scanf("%d",&size);
 
+
+    // single array 
+
+    int data[size];
+
+    // user input 
+
+    for(int a = 0 ; a < size ; a++){
+        printf("Enter element %d \n",a+1);
+        scanf("%d",&data[a]);
+    }
     
+
+
+
 
 
 
